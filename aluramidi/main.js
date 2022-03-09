878e28406605222669f81ed5ffcbe4061d9736dc
João Portela <joaoportelamadureira@gmail.com>
2022-03-09 16:16:06 -0300

    Criação do projeto feito em aula no cusro de JS para web

---
+++ aluramidi/main.js
@@ -0,0 +1,56 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    
+   
+    if(elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log("Elemento não encontrado ou seletor inválido");
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// Com while
+// let contador = 0;
+
+// while (contador < listaDeTeclas.length) {
+
+//     const tecla = listaDeTeclas[contador];
+//     const classeTeclas = tecla.classList[1]; 
+
+//     // Template string
+//     const idAudio = `#som_${classeTeclas}`
+    
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     }   
+    
+//     contador++ 
+// }
+
+// Com for 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // Template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (teclaPressionada) {
+       if (teclaPressionada.code === "Enter" || teclaPressionada.code === "Space") {
+           
+           tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');      
+    }
+    // console.log(instrumento)
+    // console.log(contador)
+}
